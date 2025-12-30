b5242cd0d382279f9ecb31a6ad42fd2398e97a06
AliAlmasiZ <ali1385almasi@gmail.com>
2025-12-30 20:44:30 +0330

    create ad models

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from .models import Ad
+
 
 # Register your models here.
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'status', 'created_at')
+    list_filter = ('status', 'category')
+    search_fields = ('title',)
+    ordering = ('-created_at',)
+
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': (
+                    'title',
+                    'description',
+                    'category',
+                    'status',
+                )
+            },
+        ),
+        (
+            'System Information', 
+            {
+                'fields': (
+                    'created_at',
+                ),
+                'classes': ('collapse',)
+            }
+        )
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                'fields': (
+                    'title',
+                    'description',
+                    'category',
+                    'status',
+                ),
+                'classes': ('wide',)
+            },
+        ),
+    )
+
+    readonly_fields = (
+        'created_at',
+    )
+
--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Ad(models.Model):
+    class AdStatus(models.TextChoices):
+        OPEN = 'open', 'Open'
+        DONE = 'done', 'Done'
+        PENDING = 'pending', 'Pending'
+    title = models.CharField(max_length=200, verbose_name="Ad Title")
+    description = models.TextField(verbose_name="Ad Description")
+    category = models.CharField(max_length=100, verbose_name="Category")
+    status = models.CharField(max_length=20, choices=AdStatus.choices, verbose_name="Ad Status")
+    owner = models.ForeignKey('accounts.User', on_delete=models.CASCADE, verbose_name="Ad Owner")
+
+    provider = models.ForeignKey('accounts.User', on_delete=models.SET_NULL, null=True, blank=True, related_name='provided_ads', verbose_name="Ad Provider")
+    
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
+
+
+    def __str__(self):
+        return self.title
