3f6ddb49cf851ca84edad4ed950d18a0463132e4
AliAlmasiZ <ali1385almasi@gmail.com>
2025-12-30 22:33:01 +0330

    add ticket models

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from .models import Ticket
 
 # Register your models here.
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("owner", "status", "related_ad", "created_at")
+    list_filter = ("owner", "status")
+    search_fields = ("onwer", )
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "owner",
+                    "status",
+                    "message",
+                    "answer",
+                    "related_ad",
+                )
+            }
+        ),
+        (
+            "System Information",
+            {
+                "fields": (
+                    "created_at",
+                )
+            }
+        )
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "owner",
+                    "status",
+                    "message",
+                    "answer",
+                    "related_ad",
+                )
+            }
+        ),
+    )
+
+    readonly_fields = (
+        "created_at", 
+    )
--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Ticket(models.Model):
+    class Status(models.TextChoices):
+        OPEN = "open", "Open"
+        PENDING = "pending", "Pending"
+        CLOSED = "closed", "Closed"
+
+    owner = models.ForeignKey("accounts.User", on_delete=models.CASCADE, related_name="tickets")
+    status = models.CharField(max_length=10, choices=Status.choices, verbose_name="Status")
+    message = models.TextField("Message")
+    answer = models.TextField("Answer", blank=True, null=True)
+    related_ad = models.ForeignKey("ads.Ad", on_delete=models.SET_NULL, related_name="tickets", blank=True, null=True)
+    created_at = models.DateTimeField("Created At", auto_now_add=True)
