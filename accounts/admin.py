2136e690da783c57863f7a44a9f55ad30dcec09f
AliAlmasiZ <ali1385almasi@gmail.com>
2025-12-30 20:26:58 +0330

    update user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,15 +1,89 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 # Register your models here.
 
 
 
-
-class CustomUserAdmin(admin.ModelAdmin):
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_staff', 'is_active')
     list_filter = ('role', 'is_staff', 'is_active')
     search_fields = ('username', 'email', 'first_name', 'last_name')
-    ordering = ('username',)
+    ordering = ('created_at',)
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': (
+                    'username',
+                    'email',
+                    'password',
+                )
+            },
+        ),
+        (
+            'Personal info',
+            {
+                'fields': (
+                    'first_name',
+                    'last_name',
+                    'phone_number',
+                    'role',
+                )
+            }
+        ),
+        (
+            'Permissions', 
+            {
+                'fields': (
+                    'is_superuser',
+                    'is_staff',
+                    'is_active',
+                    'groups', 
+                    'user_permissions'
+                )
+                
+            }
+        ),
+        (
+            'System Information', 
+            {
+                'fields': (
+                    'last_login',
+                    'created_at',
+                ),
+                'classes': ('collapse',)
+            }
+        ),
+    )
+    
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'username',
+                    'email',
+                    'first_name',
+                    'last_name',
+                    'phone_number',
+                    'role',
+                    'password1',
+                    'password2',
+                    'is_staff',
+                    'is_active',
+                    'is_superuser',
+                    'groups',
+                )
+            }
+        ),
+    )
 
-admin.site.register(User, CustomUserAdmin)
+    readonly_fields = (
+        'created_at',
+        'last_login'
+    )
+
--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,32 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import RegexValidator
 
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
 
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', 'admin')
 
+        if extra_fields.get('role') != 'admin':
+            raise ValueError('Superuser must have role=Global Admin.')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -17,14 +40,23 @@ class User(AbstractUser):
         max_length=10,
         choices=UserRole.choices,
         default=UserRole.NORMAL,
-        verbose_name=_('نقش کاربر')
+        verbose_name="User Role"
     )
-    
 
-    def get_role_display(self):
-        return self.UserRole(self.role).label
+    email = models.EmailField(unique=True, verbose_name="Email Address")
+    phone_number = models.CharField(
+        max_length=11, 
+        validators=[
+            RegexValidator("^09\d{9}$", "Phone number must be entered in the format: '09xxxxxxxxx'.")
+        ],
+        verbose_name="Phone Number"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
+
     
+    objects = UserManager()
 
     def __str__(self):
-        return f"{self.username} ({self.get_role_display()})"
+        return self.username
     
