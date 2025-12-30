e0afceccb3bc13a4807196c2c2badadb8d7e2b0c
AliAlmasiZ <ali1385almasi@gmail.com>
2025-12-30 20:55:35 +0330

    create comment models

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from .models import Comment
+
 
 # Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'ad', 'rate', 'created_at')
+    list_filter = ('rate', 'created_at')
+    search_fields = ('author__username', 'ad__title', 'content')
+    ordering = ('-created_at',)
+
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': (
+                    'author',
+                    'ad',
+                    'content',
+                    'rate',
+                )
+            },
+        ),
+        (
+            'System Information', 
+            {
+                'fields': (
+                    'created_at',
+                ),
+                'classes': ('collapse',)
+            }
+        )
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                'fields': (
+                    'author',
+                    'ad',
+                    'content',
+                    'rate',
+                ),
+                'classes': ('wide',)
+            },
+        ),
+    )
+
+    readonly_fields = (
+        'created_at',
+    )
--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Comment(models.Model):
+    author = models.ForeignKey('accounts.User', on_delete=models.CASCADE, verbose_name="Comment Author")
+    content = models.TextField(verbose_name="Comment Content")
+    rate = models.IntegerField(
+        verbose_name="Comment Rate",
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    ad = models.ForeignKey('ads.Ad', on_delete=models.CASCADE, verbose_name="Related Ad", related_name="comments")
+    provider = models.ForeignKey("accounts.Users", on_delete=models.CASCADE, verbose_name="Provider", related_name="comments")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
+
